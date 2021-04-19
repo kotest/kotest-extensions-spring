214a751e0aa37baf49a522d6e513b65e6ccee058
sksamuel <sam@sksamuel.com>
2021-04-19 10:17:32 -0500

    Migrated from main repo

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+buildscript {
+   repositories {
+      jcenter()
+      mavenCentral()
+      maven {
+         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+      }
+      maven {
+         url = uri("https://plugins.gradle.org/m2/")
+      }
+   }
+}
+
+plugins {
+   java
+   `java-library`
+   id("java-library")
+   id("maven-publish")
+   signing
+   maven
+   `maven-publish`
+   kotlin("jvm").version(Libs.kotlinVersion)
+}
+
+allprojects {
+   apply(plugin = "org.jetbrains.kotlin.jvm")
+
+   group = Libs.org
+   version = Ci.version
+
+   dependencies {
+      implementation(Libs.Kotest.api)
+      implementation(Libs.Kotest.engine)
+      implementation(Libs.Spring.context)
+      implementation(Libs.Spring.test)
+      implementation(Libs.Coroutines.coreJvm)
+      implementation(Libs.Bytebuddy.bytebuddy)
+      testImplementation(Libs.Kotest.junit5)
+   }
+
+   tasks.named<Test>("test") {
+      useJUnitPlatform()
+      testLogging {
+         showExceptions = true
+         showStandardStreams = true
+         exceptionFormat = TestExceptionFormat.FULL
+      }
+   }
+
+   tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+      kotlinOptions.jvmTarget = "1.8"
+   }
+
+   repositories {
+      mavenLocal()
+      mavenCentral()
+      maven {
+         url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+      }
+   }
+}
+
+apply("./publish.gradle.kts")
---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,28 @@
+object Libs {
+
+   const val kotlinVersion = "1.4.31"
+   const val org = "io.kotest.extensions"
+
+   object Kotest {
+      private const val version = "4.4.3"
+      const val api = "io.kotest:kotest-framework-api:$version"
+      const val engine = "io.kotest:kotest-framework-engine:$version"
+      const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
+   }
+
+   object Spring {
+      private const val version = "5.2.12.RELEASE"
+      const val context = "org.springframework:spring-context:$version"
+      const val test = "org.springframework:spring-test:$version"
+   }
+
+   object Bytebuddy {
+      const val bytebuddy = "net.bytebuddy:byte-buddy:1.10.22"
+   }
+
+   object Coroutines {
+      private const val version = "1.4.3"
+      const val coreCommon = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+      const val coreJvm = "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$version"
+   }
+}
