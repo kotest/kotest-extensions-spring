c77607d6cdca0bdf1f3b301309a8f54da165c404
sksamuel <sam@sksamuel.com>
2021-09-10 12:51:12 -0500

    Added test for soft keywords

---
+++ src/test/kotlin/org/example/myproject/import/SoftKeywordTest.kt
@@ -0,0 +1,11 @@
+package org.example.myproject.import
+
+import io.kotest.core.spec.style.StringSpec
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoftKeywordTest : StringSpec({
+
+   "empty test should always be green" {
+   }
+})
